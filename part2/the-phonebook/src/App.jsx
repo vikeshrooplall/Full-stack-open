a213a504b01ee8d729122ae2314efd4daeb35469
Vikesh Rooplall <rooplallv50@gmail.com>
2026-05-12 14:31:17 +0400

    added db.json file, installed axios library, fetched data from db.json with useEffect

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -1,24 +1,85 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name} {person.number}</p>
+  )
+}
+
+const Persons = ({ persons, searchTerm }) => {
+  // Find filtered persons
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  return (
+    <div>
+      {filteredPersons.map(person => (
+        <Person key={person.id} person={person} />
+      ))}
+    </div>
+  )
+}
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      <p>Filter shown with:</p>
+      <input
+        id='filter'
+        name='filter'
+        autoComplete='off'
+        value={filter}
+        onChange={handleFilterChange}
+      />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        Name: <input
+                id="name"
+                name="name"
+                autoComplete="name"
+                value={newName}
+                onChange={handleNameChange}
+              />
+      </div>
+      <div>
+        Number: <input
+                  id="number"
+                  name="number"
+                  autoComplete="tel"
+                  value={newNumber}
+                  onChange={handleNumberChange}
+                />
+      </div>
+      <button type='submit'>Add</button>
+    </form>
+  )
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '8046151300' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   // handle search filter
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value)
   }
-
-  // Find filtered persons
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(searchTerm.toLowerCase())
-  )
 
   const addPerson = (event) => {
     // prevent default page reload
@@ -62,38 +123,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>Filter shown with:</p>
-      <input
-        value={searchTerm}
-        onChange={handleSearchChange}
-        placeholder= 'search by name'
+      <Filter filter={searchTerm} handleFilterChange={handleSearchChange} />
+      <h3>Add a new person</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
       />
-      <form onSubmit={addPerson}>
-        <h2>Add a new</h2>
-        <div>
-          name: <input
-                  value={newName}
-                  onChange={handleNameChange}
-                />
-        </div>
-        <div>
-          number: <input
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                  />
-        </div>
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form>
       <h2>Numbers</h2>
-      {filteredPersons.map(person => {
-        return (
-          <p key={person.name}>
-            {person.name}: {person.number}
-          </p>
-        )
-      })}
+      <Persons persons={persons} searchTerm={searchTerm} />
     </div>
   )
 }
