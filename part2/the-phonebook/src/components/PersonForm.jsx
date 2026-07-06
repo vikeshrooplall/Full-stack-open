6bf896669aa59ba38e7ade0dd2827a1887d6924f
Vikesh Rooplall <rooplallv50@gmail.com>
2026-07-06 13:19:56 +0400

    display validation errors in UI

--- part2/the-phonebook/src/components/PersonForm.jsx
+++ part2/the-phonebook/src/components/PersonForm.jsx
@@ -8,6 +8,7 @@ const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNum
                 autoComplete="name"
                 value={newName}
                 onChange={handleNameChange}
+                placeholder="Enter name (min 3 characters)"
               />
       </div>
       <div>
@@ -17,6 +18,7 @@ const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNum
                   autoComplete="tel"
                   value={newNumber}
                   onChange={handleNumberChange}
+                  placeholder="e.g 09-1234556 or 040-22334455"
                 />
       </div>
       <button type='submit'>Add</button>
