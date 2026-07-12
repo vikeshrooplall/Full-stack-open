21768ea00e8a1d5ebc14c2fc38702bdfb5ddd2b9
Vikesh Rooplall <rooplallv50@gmail.com>
2026-07-12 17:57:56 +0400

    refactored app into separate modules -4.2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,35 @@
+const blogsRouter = require('express').Router()
+
+const { request } = require('express')
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,67 +1,75 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit()
-}
-
-const password = process.argv[2]
-
-const mongoUrl = `mongodb+srv://bloglist:${password}@cluster0.hpwm5ql.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Cluster0`
-mongoose.set('strictQuery',false)
-
-mongoose.connect(mongoUrl, { family: 4 })
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const express = require('express')
+// const mongoose = require('mongoose')
 
-if (process.argv.length >=6) {
-  const title = process.argv[3]
-  const author = process.argv[4]
-  const url = process.argv[5]
-  const likes = process.argv[6]
+// const app = express()
 
-  const blog = new Blog({
-    title: title,
-    author: author,
-    url: url,
-    likes: likes,
-  })
+// if (process.argv.length < 3) {
+//   console.log('give password as argument')
+//   process.exit()
+// }
 
-  blog.save().then(result => {
-    console.log('blog saved')
-    mongoose.connection.close()
-  })
-}
+// const password = process.argv[2]
+
+// const mongoUrl = `mongodb+srv://bloglist:${password}@cluster0.hpwm5ql.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Cluster0`
+// mongoose.set('strictQuery',false)
+
+// mongoose.connect(mongoUrl, { family: 4 })
+
+// const blogSchema = mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+// if (process.argv.length >=6) {
+//   const title = process.argv[3]
+//   const author = process.argv[4]
+//   const url = process.argv[5]
+//   const likes = process.argv[6]
+
+//   const blog = new Blog({
+//     title: title,
+//     author: author,
+//     url: url,
+//     likes: likes,
+//   })
+
+//   blog.save().then(result => {
+//     console.log('blog saved')
+//     mongoose.connection.close()
+//   })
+// }
 
 
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs)
+//   })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// const PORT = 3001
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
